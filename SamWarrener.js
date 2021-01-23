9e1320ea232b6edd5672072a09064e41bcadd8b3
Hugh Johnson <73081853+hughjph@users.noreply.github.com>
2021-01-23 15:27:16 +0000

    git commit -m "first commit"

---
+++ SamWarrener.js
@@ -0,0 +1,7 @@
+function Instagram(){
+    window.open("https://www.instagram.com/sam__warrener/");
+}
+
+function Spotify(){
+    window.open("https://distrokid.com/hyperfollow/samwarrener/life-in-lockdown");
+}// JavaScript Document// JavaScript Document
