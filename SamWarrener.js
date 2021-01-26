9bf2bc435fb30225eb66858beab02b5dc3123469
Hugh Johnson <73081853+hughjph@users.noreply.github.com>
2021-01-26 12:36:24 +0000

    Shows Template Page in dev Google Maps Location pinned

--- SamWarrener.js
+++ SamWarrener.js
@@ -12,3 +12,22 @@ function Spotify(){
 function PreSave() {
     window.open("https://distrokid.com/hyperfollow/samwarrener/sacrifice-my-soul");
 }
+
+function initMap() {
+    // The location of Uluru
+    const Oktoberfest = { lat: 52.997151, lng: -0.401817 };
+    // The map, centered at Uluru
+    const map = new google.maps.Map(document.getElementById("map"), {
+        zoom: 14,
+        center: Oktoberfest,
+    });
+    // The marker, positioned at Uluru
+    const marker = new google.maps.Marker({
+        position: Oktoberfest,
+        map: map,
+    });
+}
+
+
+
+//52.99838241083489, -0.4079490784180988
